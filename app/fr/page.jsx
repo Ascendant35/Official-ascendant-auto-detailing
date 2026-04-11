887e8c2c7506c230a6995c6db5dc129cfe951944
Ascendant35 <yasshac64@gmail.com>
2026-04-11 18:29:41 -0400

    Update page.jsx

--- app/fr/page.jsx
+++ app/fr/page.jsx
@@ -1,22 +1,13 @@
-import Link from "next/link";
+import Header from "../components/Header";
 
 export default function HomeFR() {
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Ascendant Auto Detailing (FR)</h1>
+    <div>
+      <Header lang="fr" />
 
-      <p>Bienvenue sur notre site.</p>
-
-      <div style={{ marginTop: "20px" }}>
-        <Link href="/fr/services">Services</Link><br />
-        <Link href="/fr/tarifs">Tarifs</Link><br />
-        <Link href="/fr/reserver">Réserver</Link><br />
-        <Link href="/fr/contact">Contactez-nous</Link><br />
-        <Link href="/fr/about">À propos</Link>
-      </div>
-
-      <div style={{ marginTop: "20px" }}>
-        <Link href="/en">Switch to English</Link>
+      <div style={{ padding: "40px" }}>
+        <h1>Ascendant Auto Detailing</h1>
+        <p>Bienvenue sur notre site.</p>
       </div>
     </div>
   );
