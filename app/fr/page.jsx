ae77eaef89df5cb572228dd83bd6deb535dc220e
Ascendant35 <yasshac64@gmail.com>
2026-04-11 18:17:08 -0400

    Update page.jsx

--- app/fr/page.jsx
+++ app/fr/page.jsx
@@ -2,22 +2,22 @@ import Link from "next/link";
 
 export default function HomeFR() {
   return (
-    <div>
-      <h1>FR Homepage</h1>
+    <div style={{ padding: "40px" }}>
+      <h1>Ascendant Auto Detailing (FR)</h1>
 
-      <p>Ascendant Auto Detailing</p>
+      <p>Bienvenue sur notre site.</p>
 
-      <div>
+      <div style={{ marginTop: "20px" }}>
         <Link href="/fr/services">Services</Link><br />
         <Link href="/fr/tarifs">Tarifs</Link><br />
         <Link href="/fr/reserver">Réserver</Link><br />
-        <Link href="/fr/contact">Contact</Link><br />
+        <Link href="/fr/contact">Contactez-nous</Link><br />
         <Link href="/fr/about">À propos</Link>
       </div>
 
-      <br />
-
-      <Link href="/en">Switch to English</Link>
+      <div style={{ marginTop: "20px" }}>
+        <Link href="/en">Switch to English</Link>
+      </div>
     </div>
   );
 }
