8089d385f03fe4536a512e9c9beed5bc17137628
Ascendant35 <yasshac64@gmail.com>
2026-04-11 18:41:15 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
