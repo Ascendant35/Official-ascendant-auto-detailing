253f094d39208f4269d564b66e277a0c9727dddc
Ascendant35 <yasshac64@gmail.com>
2026-04-13 22:48:04 -0400

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header({ lang }) {
   src="/AscendantAutoDetailing.png"
             alt="Ascendant Auto Detailing"
             style={{
-              height: "55px",
+              height: "100px",
               objectFit: "contain",
             }}
           />
