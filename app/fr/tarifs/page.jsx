17954d130e34c5fc3c20c2e535851f9b42fd6ec7
Ascendant35 <yasshac64@gmail.com>
2026-05-28 18:54:28 -0400

    Update page.jsx

--- app/fr/tarifs/page.jsx
+++ app/fr/tarifs/page.jsx
@@ -426,7 +426,7 @@ const gridSection = {
   margin: "0 auto",
   padding: "100px 50px",
   display: "grid",
-  gridTemplateColumns: "repeat(3,1fr)",
+  gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
   gap: "40px",
 };
 
@@ -504,7 +504,7 @@ const extraSection = {
 
 const extraGrid = {
   display: "grid",
-  gridTemplateColumns: "repeat(3,1fr)",
+  gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
   gap: "20px",
 };
 
